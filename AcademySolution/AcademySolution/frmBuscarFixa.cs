63f0c880cea111818be524429790704e9ce4a7a6
GladsonBruno <gladsonbruno16@gmail.com>
2017-05-27 16:37:24 -0300

    Alguns comentarios adicionados

--- AcademySolution/AcademySolution/frmBuscarFixa.cs
+++ AcademySolution/AcademySolution/frmBuscarFixa.cs
@@ -53,10 +53,11 @@ namespace AcademySolution
 
             try
             {
+                //Pega a entrada do usuario
                 String campo = txbEntrada.Text;
                 //Parametro para pegar pelo id da ficha 
                 String parametrPegaPelaFicha = "tb_record_exercises.idRecord";
-                //Lista Exercicios os exercicios da ficha
+                //Query para listar os exercicios da ficha
                 String query = "select tb_exercises.Name as NomeExercicio, tb_record_exercises.repetitions as Repeticoes, tb_record_exercises.Series as Series from tb_exercises inner join tb_record_exercises on tb_exercises.Id = tb_record_exercises.idExercise  inner join tb_records on tb_records.IdRecord = tb_record_exercises.idRecord where ";
                 query = query + parametrPegaPelaFicha + " = " + campo;
 
@@ -72,70 +73,82 @@ namespace AcademySolution
                 String stringBuscaFicha = "select * from tb_records where idRecord ";
                 stringBuscaFicha = stringBuscaFicha + " = " + campo;
 
+                //Abre conexao
                 instancia.NovaConexao();
+                //Data reader para verificar se a ficha existe
                 SqlDataReader buscaFicha = instancia.LerDados(stringBuscaFicha);
                 
+                //Se o id da ficha não for encontrado
                 if (buscaFicha.HasRows == false)
                 {
                     MetroFramework.MetroMessageBox.Show(this, "Nenhum ficha encontrada", "Erro", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
                     limpaCampos();
                 }
+                //Se o id da ficha for encontrado
                 else
-                {
+                {   //Fecha o data reader busca ficha
                     buscaFicha.Close();
 
-
+                    //Data reader que lê a lista de exercicios
                     SqlDataReader buscaExercicios = instancia.LerDados(query);
                     
-                    
+                    //Se a ficha não tiver nenhum exercicio cadastrado
                     if (buscaExercicios.HasRows == false)
                     {
+                        //Fecha o data reader para não dar conflito com os outros data readers
                         buscaExercicios.Close();
                         limpaCampos();
 
+                        //Pegga informações como nome do aluno, data de inicio e troca da ficha
                         SqlDataReader pegaInfoAluno = instancia.LerDados(infoAluno);
                         pegaInfoAluno.Read();
+                        //Preenche os campos nome, data de inicio e de troca
                         txbNomeAlunoProcuraFicha.Text = Convert.ToString(pegaInfoAluno["Nome"]);
                         txbBuscaFichaDataInicio.Text = Convert.ToString(pegaInfoAluno["dataInicio"]).Replace("00:00:00", "");
                         txbBuscaFichaDataTroca.Text = Convert.ToString(pegaInfoAluno["dataTroca"]).Replace("00:00:00", "");
-
+                        //Fecha o data reader
                         pegaInfoAluno.Close();
 
+                        //Pega o nome do professor
                         SqlDataReader pegaInfoProf = instancia.LerDados(infoProf);
                         pegaInfoProf.Read();
+                        //Preenche o nome do professor
                         txbNomeTrainerProcuraFicha.Text = Convert.ToString(pegaInfoProf["NomeProf"]);
-
+                        //Fecha o data reader
                         pegaInfoProf.Close();
-
+                        //Preenche os campos não cadastrados
                         listaExercicios.Items.Add("Nenhum Exercicio cadastrado");
                         listaRepeticoes.Items.Add("Nenhum resultado encontrado");
                         listaSeries.Items.Add("Nenhum resultado encontrado");
                         
-                    }
+                    }//Se existirem exercicios cadastrados para a ficha
                     else
                     {
                         limpaCampos();
-
+                        //Lê os exercicios e preenche o listbox exercicios
                         while (buscaExercicios.Read())
                         {
                             listaExercicios.Items.Add(Convert.ToString(buscaExercicios["NomeExercicio"]));
                             listaSeries.Items.Add(Convert.ToString(buscaExercicios["Series"]));
                             listaRepeticoes.Items.Add(Convert.ToString(buscaExercicios["Repeticoes"]));
                         }
+                        //Fecha o data reader
                         buscaExercicios.Close();
 
+                        //Le e preenche informações como nome do aluno, data de inicio e de troca da ficha
                         SqlDataReader pegaInfoAluno = instancia.LerDados(infoAluno);
                         pegaInfoAluno.Read();
                         txbNomeAlunoProcuraFicha.Text = Convert.ToString(pegaInfoAluno["Nome"]);
                         txbBuscaFichaDataInicio.Text = Convert.ToString(pegaInfoAluno["dataInicio"]).Replace("00:00:00", "");
                         txbBuscaFichaDataTroca.Text = Convert.ToString(pegaInfoAluno["dataTroca"]).Replace("00:00:00", "");
-
+                        //Fecha o data reader
                         pegaInfoAluno.Close();
 
+                        //Le o nome do professor e preenche na ficha
                         SqlDataReader pegaInfoProf = instancia.LerDados(infoProf);
                         pegaInfoProf.Read();
                         txbNomeTrainerProcuraFicha.Text = Convert.ToString(pegaInfoProf["NomeProf"]);
-
+                        //Fecha o data reader
                         pegaInfoProf.Close();
                     }
                 }           
